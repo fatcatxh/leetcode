af9d17e21ea589732594406817937eab0b12c1b6
wxh <934636512@qq.com>
2019-08-05 22:47:12 +0800

    增加注释

--- src/easy_0001_two_sum.java
+++ src/easy_0001_two_sum.java
@@ -1,6 +1,6 @@
 import java.util.HashMap;
 
-public class Solution {
+public class easy_0001_two_sum {
 
     public int[] twoSum(int nums[], int target) {
         HashMap<Integer, Integer> m1 = new HashMap();
@@ -10,10 +10,11 @@ public class Solution {
             if (m1.containsKey(diff)) {
                 return new int[]{i, m1.get(diff)};
             }
+            // 在判断之后再将值放进map，可以解决数组中有重复值的情况
             m1.put(nums[i], i);
         }
 
-
+// 遍历得出答案
 //        for (int i = 0; i < nums.length-1; i++) {
 //            for (int j = i+1; j < nums.length; j++) {
 //                if (target == nums[i] + nums[j]) {
@@ -25,7 +26,7 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        easy_0001_two_sum solution = new easy_0001_two_sum();
         int[] nums = {1,2,3,3,5};
         int[] result = solution.twoSum(nums, 7);
         for (int i : result) {
